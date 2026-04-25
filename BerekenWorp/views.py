3eb60ce188e00ff52db04de11daa32e0c40dd0a8
Jonas Brüll <jonas.bruell@gmail.com>
2026-04-25 19:35:07 +0200

    Define template page and link it to app (instead of http response)

--- BerekenWorp/views.py
+++ BerekenWorp/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def bereken_worp(request):
-    return HttpResponse("Hello, world!")
-
+    return render(request, 'BerekenWorp.html')
